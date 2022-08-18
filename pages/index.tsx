a588a46bff0a3bed41d968e8910c9cab352839c9
Funncy <gywns41@naver.com>
2022-08-18 21:46:21 +0900

    [Modifed] 로그인 가능하게 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,83 @@
-import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/configureStore';
+import type {NextPage} from 'next';
+import {useEffect, useState} from 'react';
+import {useRouter} from 'next/router';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/configureStore';
 import styled from 'styled-components';
-import {
-  CalendarOutlined,
-  OrderedListOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+import {CalendarOutlined, OrderedListOutlined, UserOutlined,} from '@ant-design/icons';
 import BottomNavItem from '../components/bottomNavItem';
 import HomeBody from '../components/HomeBody';
 
 const Home: NextPage = () => {
-  const [selectedNav, setSelectedNav] = useState(1);
-  const { uid, token } = useSelector((state: RootState) => state.auth);
-  const router = useRouter();
+    const [selectedNav, setSelectedNav] = useState(1);
+    const {uid, token} = useSelector((state: RootState) => state.auth);
+    const router = useRouter();
 
-  const BodyWrapper = styled.div`
-    margin: auto;
-    max-width: 400px;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    //background-color: green;
-  `;
 
-  const BottomNav = styled.div`
-    box-shadow: 0 0 5px #ebeff6;
-    background-color: white;
-    height: 40px;
-    max-width: 400px;
-    width: 100%;
-    margin: auto;
-    display: flex;
-    font-size: 10px;
-  `;
+    const BodyWrapper = styled.div`
+      margin: auto;
+      max-width: 400px;
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      //background-color: green;
+    `;
 
-  const ContentWrapper = styled.div`
-    flex: 1;
-  `;
+    const BottomNav = styled.div`
+      box-shadow: 0 0 5px #ebeff6;
+      background-color: white;
+      height: 40px;
+      max-width: 400px;
+      width: 100%;
+      margin: auto;
+      display: flex;
+      font-size: 10px;
+    `;
 
-  useEffect(() => {
-    // if (uid === null) {
-    //   router.push('/login');
-    // }
-  }, [uid]);
+    const ContentWrapper = styled.div`
+      flex: 1;
+    `;
 
-  if (uid !== null) return <div></div>;
+    useEffect(() => {
+        if (uid === null) {
+            router.push('/login');
+        }
+    }, [uid]);
 
-  return (
-    <div>
-      <BodyWrapper>
-        <ContentWrapper>
-          <HomeBody />
-        </ContentWrapper>
-        <BottomNav>
-          <BottomNavItem
-            active={selectedNav === 1}
-            onClick={(e) => setSelectedNav(1)}
-          >
-            <OrderedListOutlined style={{ fontSize: '15px' }} />
-            <span style={{ fontSize: '4px', fontWeight: 'bold' }}>홈</span>
-          </BottomNavItem>
-          <BottomNavItem
-            active={selectedNav === 2}
-            onClick={(e) => setSelectedNav(2)}
-          >
-            <CalendarOutlined style={{ fontSize: '15px' }} />
-            <span>캘린더</span>
-          </BottomNavItem>
-          <BottomNavItem
-            active={selectedNav === 3}
-            onClick={(e) => setSelectedNav(3)}
-          >
-            <UserOutlined style={{ fontSize: '15px' }} />
-            <span>프로필</span>
-          </BottomNavItem>
-        </BottomNav>
-      </BodyWrapper>
-    </div>
-  );
+    if (uid == null) return <div></div>;
+
+    return (
+        <div>
+            <BodyWrapper>
+                <ContentWrapper>
+                    <HomeBody/>
+                </ContentWrapper>
+                <BottomNav>
+                    <BottomNavItem
+                        active={selectedNav === 1}
+                        onClick={(e) => setSelectedNav(1)}
+                    >
+                        <OrderedListOutlined style={{fontSize: '15px'}}/>
+                        <span style={{fontSize: '4px', fontWeight: 'bold'}}>홈</span>
+                    </BottomNavItem>
+                    <BottomNavItem
+                        active={selectedNav === 2}
+                        onClick={(e) => setSelectedNav(2)}
+                    >
+                        <CalendarOutlined style={{fontSize: '15px'}}/>
+                        <span>캘린더</span>
+                    </BottomNavItem>
+                    <BottomNavItem
+                        active={selectedNav === 3}
+                        onClick={(e) => setSelectedNav(3)}
+                    >
+                        <UserOutlined style={{fontSize: '15px'}}/>
+                        <span>프로필</span>
+                    </BottomNavItem>
+                </BottomNav>
+            </BodyWrapper>
+        </div>
+    );
 };
 
 export default Home;
